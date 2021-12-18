9045d398608a8969e6ace69f26e455d218c27225
Stephen Johnson <stephenjayyy@gmail.com>
2021-12-18 05:07:52 -0600

    Adds Asset class and frameworks

---
+++ AssetTrack/Models/Asset.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssetTrack.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public string Date { get; set; }
+        public string Location { get; set; }
+
+        [DisplayName("Part Type")]
+        public string PartType { get; set; }
+
+        public string MSF { get; set; }
+
+        [DisplayName("Part Number")]
+        public string PartNumber { get; set; }
+
+        [DisplayName("Serial Number")]
+        public string? SerialNumber { get; set; }
+
+        [DisplayName("Task Number")]
+        public string? TaskNumber { get; set; }
+
+        public string? Notes { get; set; }
+        public string User { get; set; }
+
+        public Asset()
+        {
+
+        }
+    }
+}
