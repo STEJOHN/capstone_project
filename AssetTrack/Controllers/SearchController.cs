b42b06c9816676d1dddd55ab51d97b2188a6da2c
Stephen Johnson <stephenjayyy@gmail.com>
2021-12-30 16:44:38 -0600

    Adds search function / controller

---
+++ AssetTrack/Controllers/SearchController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AssetTrack.Data;
+using AssetTrack.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AssetTrack.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly AssetTrackContext _context;
+
+        public SearchController(AssetTrackContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
+        }
+
+        [Authorize]
+        // GET: Asset
+        public async Task<IActionResult> Index(string searchString)
+        {
+            //Using LINQ to create query 
+            var assets = from t in _context.Asset
+                         select t;
+
+
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                assets = assets.Where(i => i.Location!.Contains(searchString) ||
+
+                i.Date!.ToString().Contains(searchString) ||
+                i.User!.Contains(searchString) ||
+                i.Notes!.Contains(searchString) ||
+                i.PartNumber!.Contains(searchString) ||
+                i.PartType!.Contains(searchString) ||
+                i.SerialNumber!.Contains(searchString) ||
+                i.TaskNumber!.Contains(searchString
+
+                ));
+            }
+
+
+            //return View(await _context.Things.ToListAsync());
+            return View(await assets.ToListAsync());
+        }
+        [Authorize]
+        // GET: Search/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asset = await _context.Asset
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+
+            return View(asset);
+        }
+        [Authorize]
+        // GET: Search/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Search/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Date,Location,PartType,MSF,PartNumber,SerialNumber,TaskNumber,Notes,User")] Asset asset)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(asset);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(asset);
+        }
+
+        [Authorize]
+        // GET: Search/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asset = await _context.Asset.FindAsync(id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+            return View(asset);
+        }
+
+        [Authorize]
+        // POST: Search/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Location,PartType,MSF,PartNumber,SerialNumber,TaskNumber,Notes,User")] Asset asset)
+        {
+            if (id != asset.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(asset);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssetExists(asset.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(asset);
+        }
+
+        [Authorize]
+        // GET: Search/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asset = await _context.Asset
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+
+            return View(asset);
+        }
+
+        [Authorize]
+        // POST: Search/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var asset = await _context.Asset.FindAsync(id);
+            _context.Asset.Remove(asset);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize]
+        private bool AssetExists(int id)
+        {
+            return _context.Asset.Any(e => e.Id == id);
+        }
+    }
+}
